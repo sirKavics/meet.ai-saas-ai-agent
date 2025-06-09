376ee3165e185b419d4a4a848e61ba2c0f87620e
sirkavics <falf555@gmail.com>
2025-06-08 23:11:56 -0400

    18: meetings data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,12 +4,13 @@ import { useRouter } from "next/navigation";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 import { useTRPC } from "@/trpc/client";
+import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
+import { EmptyState } from "@/components/empty-state";
+
 
 import { columns } from "../components/columns";
-import { DataTable } from "../components/data-table";
-import { EmptyState } from "@/components/empty-state";
 import { DataPagination } from "../components/data-pagination";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 
